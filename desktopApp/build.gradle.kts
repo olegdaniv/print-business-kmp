0aaf1130be14608e838639dceba4c071ce628407
Oleg <oleg.daniv@newsoft-ns.com>
2026-03-07 20:12:44 +0200

    feat: Add Kotlin Serialization plugin and update API host, port, and scheme to remote HTTPS.

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.kotlinJvm)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.kotlinSerialization)
 }
 
 group = "com.printbusinesskmp"
